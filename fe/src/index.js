8f9ece9d4172ee24e115f341abd0ceed5b471230
Ivan Giuliani <giuliani.v@gmail.com>
2018-10-28 00:04:20 +0100

    Sample react app

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,10 +1,7 @@
-function component() {
-  let element = document.createElement('div');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(
+  React.createElement("div", null, "Hello World"), // <div>Hello World</div>
+  document.getElementById("root")
+);
