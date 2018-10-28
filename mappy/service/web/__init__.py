c36c8935026fb987453b73db684ec9977224c43e
Ivan Giuliani <giuliani.v@gmail.com>
2018-10-28 18:16:12 +0000

    Enable cors on all routes

--- mappy/service/web/__init__.py
+++ mappy/service/web/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from mappy import config
 from . import api, frontend
@@ -15,6 +16,7 @@ def create_app(app_name=__name__):
     app = Flask(app_name,
                 template_folder=config.Web.TEMPLATE_ROOT,
                 static_folder=config.Web.STATIC_FILES_PATH)
+    CORS(app)
 
     for blueprint in blueprints():
         app.register_blueprint(blueprint)
