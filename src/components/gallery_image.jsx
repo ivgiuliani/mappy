16f3a52b11825f033d93d66706444cb2c41ca764
Ivan Giuliani <giuliani.v@gmail.com>
2018-10-28 17:08:07 +0000

    Better url construction

--- src/components/gallery_image.jsx
+++ src/components/gallery_image.jsx
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 
 class GalleryImage extends Component {
-  state = {
-    album_id: null,
-    image_id: null
-  };
-
   render() {
     return (
-      <img
-        className="gallery-image"
-        width="200px"
-        src={
-          "http://localhost:5000/serve/thumb/album/" +
-          this.state.album_id +
-          "/image/" +
-          this.state.image_id
-        }
-      />
+      <img className="gallery-image" width="200px" src={this.thumbnailUrl()} />
+    );
+  }
+
+  thumbnailUrl() {
+    return (
+      MAPPY_API_HOST +
+      "/serve/thumb/album/" +
+      this.props.album_id +
+      "/image/" +
+      this.props.image_id
     );
   }
 }
