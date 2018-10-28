1d41aeac96ad08013275e3b74421e7154bd8e172
Ivan Giuliani <giuliani.v@gmail.com>
2018-10-28 01:03:05 +0100

    Serve images

--- mappy/service/web/frontend.py
+++ mappy/service/web/frontend.py
@@ -1,8 +1,26 @@
-from flask import Blueprint, render_template
+import os
+
+from flask import Blueprint, abort, send_from_directory
+
+from mappy.service import albums
 
 blueprint = Blueprint("frontend", __name__, url_prefix="/")
 
 
-@blueprint.route("/")
-def root():
-    return render_template("index.html")
+@blueprint.route("/serve/thumb/album/<aid>/image/<iid>")
+def serve_thumb(aid, iid):
+    if not albums.Album.exists(aid):
+        abort(404)
+
+    album = albums.Album.load(aid)
+    # TODO: actually send a thumbnail
+    return send_from_directory(album.path, iid)
+
+
+@blueprint.route("/serve/full/album/<aid>/image/<iid>")
+def serve(aid, iid):
+    if not albums.Album.exists(aid):
+        abort(404)
+
+    album = albums.Album.load(aid)
+    return send_from_directory(album.path, iid)
