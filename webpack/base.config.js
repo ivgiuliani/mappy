2d8de6725e46fda353ff9555630f19f792a277a9
Ivan Giuliani <giuliani.v@gmail.com>
2018-10-28 17:14:46 +0000

    Add css loader

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -36,6 +36,18 @@ module.exports = {
         test: /\.jsx?$/,
         loaders: ["babel-loader"],
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+        query: {
+          modules: true,
+          localIdentName: "[name]__[local]___[hash:base64:5]"
+        }
       }
     ]
   }
