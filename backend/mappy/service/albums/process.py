ea2330d21ca6c9b63b87b68d63f15af9b14057d5
Ivan Giuliani <giuliani.v@gmail.com>
2018-11-03 21:34:03 +0000

    Reuse the closest location when one is missing

--- backend/mappy/service/albums/process.py
+++ backend/mappy/service/albums/process.py
@@ -2,7 +2,8 @@ from mappy import utils
 
 from dateutil import parser
 
-TRIANGULATE_GEOLOCATION_THRESHOLD_SECONDS = 3 * 60  # 3 minutes
+TRIANGULATE_GEOLOCATION_THRESHOLD_SECONDS = 3 * 60
+APPROXIMATE_GEOLOCATION_THRESHOLD_SECONDS = 80
 
 
 def triangulate(prev_img, next_img):
@@ -28,19 +29,39 @@ def augment_geolocation(prev_img, curr_img, next_img):
     if not prev_img or not next_img:
         return curr_img
 
-    if not prev_img["has_geolocation"] or not next_img["has_geolocation"]:
+    if not prev_img["has_geolocation"] and not next_img["has_geolocation"]:
         return curr_img
 
+    def augment(d, lat, lng):
+        d.update({
+            "has_geolocation": True,
+            "estimated_location": True,
+            "lat": lat,
+            "lng": lng,
+        })
+
+    curr_dt = parser.parse(curr_img["date_time"])
     prev_dt = parser.parse(prev_img["date_time"])
     next_dt = parser.parse(next_img["date_time"])
-    time_diff = (next_dt - prev_dt).seconds
+    if prev_img["has_geolocation"] and next_img["has_geolocation"]:
+        # Both prev and next have a geolocation, use the avg point in this case
+        time_diff = (next_dt - prev_dt).seconds
 
-    if time_diff < TRIANGULATE_GEOLOCATION_THRESHOLD_SECONDS:
-        est_lat, est_lng = triangulate(prev_img, next_img)
-        curr_img["has_geolocation"] = True
-        curr_img["estimated_location"] = True
-        curr_img["lat"] = est_lat
-        curr_img["lng"] = est_lng
+        if time_diff < TRIANGULATE_GEOLOCATION_THRESHOLD_SECONDS:
+            est_lat, est_lng = triangulate(prev_img, next_img)
+            augment(curr_img, est_lat, est_lng)
+    elif prev_img["has_geolocation"]:
+        # Only the previous image has geolocation data
+        time_diff = (curr_dt - prev_dt).seconds
+
+        if time_diff < APPROXIMATE_GEOLOCATION_THRESHOLD_SECONDS:
+            augment(curr_img, prev_img["lat"], prev_img["lng"])
+    else:
+        # Only the next image has geolocation data
+        time_diff = (next_dt - curr_dt).seconds
+
+        if time_diff < APPROXIMATE_GEOLOCATION_THRESHOLD_SECONDS:
+            augment(curr_img, next_img["lat"], next_img["lng"])
 
     return curr_img
 
