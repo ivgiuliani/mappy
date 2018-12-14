4dabebe5ae823607074e566a3f32737b012450ff
Ivan Giuliani <giuliani.v@gmail.com>
2018-12-14 20:43:30 +0000

    Force click to start under python3

--- backend/start-dev.sh
+++ backend/start-dev.sh
@@ -3,7 +3,9 @@
 FLASK_DEBUG=1
 FLASK_APP="mappy.service.web:create_app('mappy')"
 
-env LC_ALL=C \
+env \
+  LC_ALL=C.UTF-8 \
+  LANG=C.UTF-8 \
   FLASK_DEBUG=$FLASK_DEBUG \
   FLASK_APP=$FLASK_APP \
   flask run
